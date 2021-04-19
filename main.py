9d9065b2dfb5b4a41e137d6835f7a045bbd3b353
dagz214 <58767064+dagz214@users.noreply.github.com>
2021-04-19 16:36:03 +0300

    Implenented looning for profiles on external .yaml file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from simplegmail import Gmail
 from simplegmail.query import construct_query
+import yaml
 import os
 
-BASE_PATH = "."
+BASE_PATH = "./saves"
 
 
 def check_if_path_exists_or_create(path: str) -> bool:
@@ -16,11 +17,11 @@ def check_if_path_exists_or_create(path: str) -> bool:
     if os.path.exists(path):
         return True
     else:
-        os.mkdir(path)
+        os.makedirs(path, exist_ok=True)
         return True
 
 
-def get_messages(gmail, download=True, **kwargs):
+def get_messages(gmail, description, download=True, **kwargs):
     """
 
     Main function to get messages and download attchments.
@@ -32,51 +33,56 @@ def get_messages(gmail, download=True, **kwargs):
         
     """
     query_params = {}
+
+    # get the months to look back and add to query
+    months_back=kwargs.pop('months_back')
+    query_params['newer_than']=(months_back, "month")
+
+    # parse the remaining kwargs for the query
     for key, value in kwargs.items():
         query_params[key] = value
 
     messages = gmail.get_messages(query=construct_query(query_params))
-
+    
     # check if attachment should be download an iterate messages:
     if download == True:
         for message in messages:
-            download_attachments(message)
+            download_attachments(message, description)
 
     return messages
 
 
-def download_attachments(message):
+def download_attachments(message, description):
+    """
+
+    Downloads attachments from a single message
+
+    """
     if message.attachments:
-        for attm in message.attachments:
-            print("File: " + attm.filename)
-            attm.save()
+        month=message.date[5:7]
+        year=message.date[0:4]
+        # path = BASE_PATH
+        path = f'{BASE_PATH}/{year}_{month}'
+        if check_if_path_exists_or_create(path):
+            for attm in message.attachments:
+                # print("File: " + attm.filename)
+                attm.save(filepath=(
+                    f'{path}/'
+                    f'{year}'
+                    '_'
+                    f'{month}'
+                    ' '
+                    f'{description}'
+                    f'{attm.filename[-4:]}'
+                ))
 
 
 gmail = Gmail()
-messages = get_messages(
-    gmail,
-    download=True,
-    newer_than=(3, "month"),
-    sender="microsoft-noreply@microsoft.com",
-    attachment=True,
-)
-# messages = get_microsoft_invoices(gmail)
-# check_if_path_exists_or_create(f'{BASE_PATH}/saves/')
-# download_attachments(messages)
-# # Unread messages in your inbox
-# messages = gmail.get_unread_inbox()
 
-# # Starred messages
-# messages = gmail.get_starred_messages()
+#open the external profiles yaml file
+with open(r'.\profiles.yaml') as file:
+    profile_list = yaml.load(file, Loader=yaml.FullLoader)
 
-# # ...and many more easy to use functions can be found in gmail.py!
+for profile in profile_list:
+    get_messages(gmail, list(profile.keys())[0], **list(profile.values())[0])
 
-# # Print them out!
-# for message in messages:
-#     print("To: " + message.recipient)
-#     print("From: " + message.sender)
-#     print("Subject: " + message.subject)
-#     print("Date: " + message.date)
-#     print("Preview: " + message.snippet)
-
-#     print("Message Body: " + message.plain)  # or message.html
